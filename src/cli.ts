f143323447350f3d73ac6cbc84720ed25df7bcb1
Kim Yewon <kyw017763@gmail.com>
2021-02-26 21:44:22 +0900

    Add user profile print, inquirer

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,6 @@
 import inquirer from 'inquirer'
+import terminalImage from 'terminal-image'
+import axios from 'axios'
 import * as Github from './lib/github'
 
 export default async () => {
@@ -12,4 +14,40 @@ export default async () => {
         ])
     
     const user = await Github.getProfile(token)
+
+    const {
+        login,
+        avatar_url,
+        url,
+        name,
+        bio,
+        public_repos,
+        total_private_repos
+    } = user
+    let { created_at: createdAt, updated_at: updatedAt } = user
+    createdAt = new Date(createdAt).toISOString().substr(0, 10)
+    updatedAt = new Date(updatedAt).toISOString().substr(0, 10)
+
+    const responseImage = (await axios.get(avatar_url, { responseType: 'arraybuffer' }))
+    const image = Buffer.from(responseImage.data, 'binary')
+
+    console.log(await terminalImage.buffer(image, { width: '50%', height: '50%' }))
+    console.log(`[${url}]`)
+    console.log(`${login} (${name})`)
+    console.log(`${bio}`)
+    console.log(`${public_repos} public repos & ${total_private_repos} private_repos`)
+    console.log(`Created at ${createdAt} & Updated at ${updatedAt}`)
+
+    const { isCorrectUser } = await inquirer
+        .prompt([
+            {
+                type: 'confirm',
+                name: 'isCorrectUser',
+                message: 'Is this profile yours?'
+            }
+        ])
+    
+    if (!isCorrectUser) {
+        return null
+    }
 }
